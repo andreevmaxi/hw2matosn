e178920cbe29bd0a583456df0c1fd6231addf62e
Maksimka <52958787+andreevmaxi@users.noreply.github.com>
2019-09-20 16:55:22 +0300

    2.0

--- main.cpp
+++ main.cpp
@@ -1,30 +1,41 @@
 #include <cstdio>
 #include <cassert>
+#include <cstdlib>
 
-unsigned long long allocation(unsigned long long n, unsigned long long k);
+#ifdef _VORONEZH
+    #define CALL printf
+#else
+    #define CALL if(0) printf
+#endif
 
-unsigned long long combinations(unsigned int n, unsigned int k);
+unsigned long long   allocation(unsigned long long n, unsigned long long k);
+
+unsigned long long combinations(unsigned long long n, unsigned long long k);
 
 int main()
 {
     char operation = {};
     unsigned long long n,k;
-    scanf("%c %d %d", &operation, &n, &k);
+    CALL("Hello, zemlyak, enter your operation and two integers, used in this operation\n");
+    scanf("%c %llu %llu", &operation, &n, &k);
 
     assert(n != 0);
     assert(k != 0);
 
     switch (operation)
     {
-        case 'Ñ':
-            printf("%d", combinations(n, k));
+        case 'C':
+            CALL("Number of combinations of k in n: ");
+            printf("%llu", combinations(n, k));
             break;
         case 'A':
-            printf("%d", allocation(n, k));
+            CALL("Number of allocations of k in n: ");
+            printf("%llu", allocation(n, k));
             break;
         default:
             printf("error");
     }
+    CALL("\nThank you for reading this code!");
 
     return 0;
 }
@@ -35,23 +46,22 @@ unsigned long long allocation(unsigned long long n, unsigned long long k)
     assert(n > k);
     unsigned long long answer = 1;
     unsigned long long tmp = n - k;
-    printf("%d", tmp);
 
     for (unsigned long long CounterFactorial = n; CounterFactorial > tmp; --CounterFactorial)
     {
-        printf("%d %d %d\n", CounterFactorial, (CounterFactorial > tmp), tmp);
         answer *= CounterFactorial;
     }
 
     return answer;
 }
 
-unsigned long long combinations(unsigned int n, unsigned int k)
+unsigned long long combinations(unsigned long long n, unsigned long long k)
 {
-    long long answer = 1;
-    int tmp = 0;
+    unsigned long long answer = 1;
+    unsigned long long tmp = 0;
+    unsigned long long DivideTmp = 2;
 
-    if(k < (n - k))
+    if(k < (n - k))  //swaps k and (n - k) to use it in n!/k!*(n - k)!
     {
         tmp = (n - k);
     } else
@@ -60,14 +70,43 @@ unsigned long long combinations(unsigned int n, unsigned int k)
         k = (n - k);
     }
 
-    for (int CounterFactorial = 0; CounterFactorial < tmp; ++CounterFactorial)
+    unsigned long long* FactorArray = (unsigned long long*)calloc((n - tmp), sizeof(unsigned long long));
+    unsigned long long* FactorialOfKArray = (unsigned long long*)calloc(k - 1, sizeof(unsigned long long));
+
+    int i = 0; // index integer of FactorArray
+
+    for (unsigned long long CounterFactorial = n; CounterFactorial > tmp; --CounterFactorial)
     {
-        answer *= (n - CounterFactorial);
+        *(FactorArray + i) = CounterFactorial;
+        CALL("FactorArray: %llu %d\n", *(FactorArray + i), i);
+        ++i;
     }
-    for (int i = 2; i < k; ++i)
+
+    for (int j = 0; j < k - 1; ++j)
     {
-        answer /= i;
+        *(FactorialOfKArray + j) = j + 2;
+        CALL("FactorialOfKArray: %llu %d\n", *(FactorialOfKArray + j), j);
     }
+
+    CALL("\n");
+
+    for (i = 0; i < (n - tmp); ++i)
+    {
+        for (int j = 0; j < k - 1; ++j)
+        {
+            if(*(FactorialOfKArray + j) != 0 && *(FactorArray + i) % *(FactorialOfKArray + j) == 0)
+            {
+                CALL("%llu %llu\n", *(FactorArray + i), *(FactorialOfKArray + j));
+                *(FactorArray + i) /= *(FactorialOfKArray + j); // doing ans/k! or ans/(n - k)! if k > (n - k)
+                *(FactorialOfKArray + j) = 0;
+            }
+        }
+
+        answer *= (*(FactorArray + i)); // doing n!/(n - k)! or n!/k! if k > (n - k)
+    }
+
+    free(FactorArray);
+    free(FactorialOfKArray);
 
     return answer;
 }
